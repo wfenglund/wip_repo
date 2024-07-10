77364a61b13006083d09a946b3110e1120d37cae
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-07-10 22:22:16 +0200

    fixed so that every query gets its own out file

--- primer_snp_pipeline/pipeline/2_get_sequences/2_get_sequences.sh
+++ primer_snp_pipeline/pipeline/2_get_sequences/2_get_sequences.sh
@@ -1,12 +1,12 @@
 # Load software modules:
 module load bioinfo-tools blast
 
-# create output file:
-touch seqs_hits.fa
-
 # get sequences:
 while read hit
 do
-	query=`echo $hit | awk '{print "-entry " $2 " -range " $3 "-" $4}'`
-	blastdbcmd -db ../1_blast/$BLAST_DB $query >> seqs_hits.fa
+	query=`echo $hit | awk '{print "-entry " $2 " -range " ($3 - 100) "-" ($4 + 100)}'`
+	file_name=`echo $hit | awk '{print "./fasta_files/" $1 ".fa"}'`
+	touch $file_name
+	echo $query
+	blastdbcmd -db ../1_blast/$BLAST_DB $query >> $file_name
 done < ../1_blast/blast.out
