a09fdf498c09028ef633a4123304aeddc9306cf9
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-07-10 23:15:17 +0200

    fixed indentation

--- primer_snp_pipeline/pipeline/2_get_sequences/2_get_sequences.sh
+++ primer_snp_pipeline/pipeline/2_get_sequences/2_get_sequences.sh
@@ -4,9 +4,9 @@ module load bioinfo-tools blast
 # get sequences:
 while read hit
 do
-	query=`echo $hit | awk '{print "-entry " $2 " -range " ($3 - 100) "-" ($4 + 100)}'`
-	file_name=`echo $hit | awk '{print "./fasta_files/" $1 ".fa"}'`
-	touch $file_name
-	echo $query
-	blastdbcmd -db ../1_blast/$BLAST_DB $query >> $file_name
+  query=`echo $hit | awk '{print "-entry " $2 " -range " ($3 - 100) "-" ($4 + 100)}'`
+  file_name=`echo $hit | awk '{print "./fasta_files/" $1 ".fa"}'`
+  touch $file_name
+  echo $query
+  blastdbcmd -db ../1_blast/$BLAST_DB $query >> $file_name
 done < ../1_blast/blast.out
