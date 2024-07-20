2935191ee0b515a9d8bb338d1a191a0ffa093b2e
wfenglund <w.f.englund@gmail.com>
2024-07-20 22:47:00 +0200

    rewrote script so that it checks for range accuracy

--- primer_snp_pipeline/pipeline/2_get_sequences/2_get_sequences.sh
+++ primer_snp_pipeline/pipeline/2_get_sequences/2_get_sequences.sh
@@ -4,9 +4,17 @@ module load bioinfo-tools blast
 # get sequences:
 while read hit
 do
-  query=`echo $hit | awk '{print "-entry " $2 " -range " ($3 - 100) "-" ($4 + 100)}'`
-  file_name=`echo $hit | awk '{print "./fasta_files/" $1 ".fa"}'`
-  touch $file_name
-  echo $query
-  blastdbcmd -db ../1_blast/$BLAST_DB $query >> $file_name
+  cur_seq=`echo $hit | awk '{print $1}'`
+  range_fr=`echo $hit | awk '{print $3}'`
+  range_to=`echo $hit | awk '{print $4}'`
+  if [ $range_to -gt $range_fr ]
+  then
+    query=`echo $hit | awk '{print "-entry " $2 " -range " ($3 - 100) "-" ($4 + 200)}'`
+    file_name="./fasta_files/$cur_seq.fa"
+    touch $file_name
+    echo "Running $query"
+    blastdbcmd -db ../1_blast/$BLAST_DB $query >> $file_name
+  else
+    echo "Start is larger than stop for sequence $cur_seq"
+  fi
 done < ../1_blast/blast.out
