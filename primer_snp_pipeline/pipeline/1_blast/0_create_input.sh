c489edb62d95ad77b18f79f7e7c05015e9d796d1
wfenglund <w.f.englund@gmail.com>
2024-07-20 22:47:00 +0200

    added a script for parsing raw file and creating input

---
+++ primer_snp_pipeline/pipeline/1_blast/0_create_input.sh
@@ -0,0 +1,12 @@
+# Extract sequences and forward primers from csv-file with awk and sed (replacing the snp with an N).
+#
+# Forward primer will be written in the fasta sequence name after a '|'.
+#
+# This script assumes that:
+# 1. The input is a semi-colon separated file (with a header, since first line will be ignored).
+# 2. That the name of the sequence is located in column 3
+# 3. That the sequence is located in column 9
+# 4. That the forward primer is located in column 12
+#
+# Generate fasta file:
+tail -n +2 Copy\ of\ Ua_2018_8941FSTP18T1_DESIGN.csv | awk 'BEGIN{FS=";"};{print ">"$3"|"$12 "\n" $9}' | sed 's/\[\w\/\w\]/N/g' > sequences.fa
