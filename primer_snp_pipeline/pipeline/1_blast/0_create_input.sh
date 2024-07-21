1c79e60e7e77efac5dc340678b68d4cbf94a5c87
wfenglund <w.f.englund@gmail.com>
2024-07-21 21:34:00 +0200

    fixed so that p3 inputs are generated with fw primer set

--- primer_snp_pipeline/pipeline/1_blast/0_create_input.sh
+++ primer_snp_pipeline/pipeline/1_blast/0_create_input.sh
@@ -1,6 +1,6 @@
 # Extract sequences and forward primers from csv-file with awk and sed (replacing the snp with an N).
 #
-# Forward primer will be written in the fasta sequence name after a '|'.
+# Forward primer will be written in the fasta sequence name after three underscores '___'.
 #
 # This script assumes that:
 # 1. The input is a semi-colon separated file (with a header, since first line will be ignored).
@@ -9,4 +9,4 @@
 # 4. That the forward primer is located in column 12
 #
 # Generate fasta file:
-tail -n +2 Copy\ of\ Ua_2018_8941FSTP18T1_DESIGN.csv | awk 'BEGIN{FS=";"};{print ">"$3"|"$12 "\n" $9}' | sed 's/\[\w\/\w\]/N/g' > sequences.fa
+tail -n +2 Copy\ of\ Ua_2018_8941FSTP18T1_DESIGN.csv | awk 'BEGIN{FS=";"};{print ">"$3"___"$12 "\n" $9}' | sed 's/\[\w\/\w\]/N/g' > sequences.fa
--- primer_snp_pipeline/pipeline/3_make_input/3_make_p3_input.sh
+++ primer_snp_pipeline/pipeline/3_make_input/3_make_p3_input.sh
@@ -1,25 +1,31 @@
-# Load software modules:
 module load bioinfo-tools
 module load emboss
 
-# Create one primer3 input file per fasta file:
 for fasta_file in ../2_get_sequences/fasta_files/*.fa
 do
-  fasta_name=${fasta_file/".fa"/""}
-  fasta_name=${fasta_name/"../2_get_sequences/fasta_files/"/""}
-  output="./input_files/"$fasta_name"_p3_input.txt"
-  touch $output
+  # parse sequence info:
+  fasta_raw=${fasta_file/".fa"/""}
+  fasta_raw=${fasta_raw/"../2_get_sequences/fasta_files/"/""}
+  fasta_name=`echo $fasta_raw | awk 'BEGIN{FS="___"};{print $1}'`
+  output="./generated_inputs/"$fasta_name"_p3_input.txt"
+  fw_primer=`echo $fasta_raw | awk 'BEGIN{FS="___"};{print $2}'`
+  
+  # get sequence(s), make consensus if there are several:
   nseq=`grep ">" $fasta_file | wc -l`
-  if [ $nseq -gt 1 ] # if fasta file contains several sequences
+  if [ $nseq -gt 1 ] # if there are more than one sequence in file
   then
     echo $fasta_file
-    conseq=`cons -sequence $fasta_file -stdout -auto | tail -n +2`
-  elif [ $nseq == 1 ] # if fasta file contains exactly one sequence
+    conseq=`cons -sequence "$fasta_file" -stdout -auto | tail -n +2`
+  elif [ $nseq == 1 ] # if there is only one sequence in file
   then
-    conseq=`cat $fasta_file | tail -n +2` # take the sequence and not the name
+    conseq=`cat $fasta_file | tail -n +2`
   fi
   conseq=`echo $conseq | tr -d '[:blank:]'`
+
+  # write output:
+  touch $output
   echo "SEQUENCE_ID=$fasta_name" >> $output
   echo "SEQUENCE_TEMPLATE=$conseq" >> $output
+  echo "SEQUENCE_PRIMER=$fw_primer" >> $output
   cat primer3_settings.txt >> $output
 done
