395ac3990f71eef3d6f5048e52bf0d2c7cb7ddcc
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-07-25 22:13:47 +0200

    fix so that p3 is run for every input file and writes a log

--- primer_snp_pipeline/pipeline/4_run_primer3/4_run_primer3.sh
+++ primer_snp_pipeline/pipeline/4_run_primer3/4_run_primer3.sh
@@ -1,3 +1,14 @@
 module load bioinfo-tools Primer3
 
-primer3_core < ../3_make_p3_input/generated_inputs/file_p3_input.txt
+# run primer3 for every input file:
+run_p3()
+{
+  for input_file in ../3_make_p3_input/generated_inputs/*
+  do
+    primer3_core < $input_file
+  done
+}
+run_p3 | tee log.out # run script and also save output to log.out
+
+# move output files to output folder:
+mv *.rev ./output_files/
